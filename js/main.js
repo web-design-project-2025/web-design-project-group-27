06324f069f86c86a5151b870ee759537e9d9ced5
nivinmuhamed <muni24by@student.ju.se>
2025-04-24 13:26:25 +0200

    - created a new folder for the category pictures - changed the JSON code so that we can interact with the pictures - added an eventListener i the main.js with if statements so the pictures are interactable and take us to another page - next step is to make it into sliding menu

--- js/main.js
+++ js/main.js
@@ -17,9 +17,29 @@ fetch("data/categories.json")
       card.appendChild(img);
       card.appendChild(title);
       container.appendChild(card);
+
+      card.addEventListener("click", () => {
+        let pageName =
+          category.name
+            .toLowerCase()
+            .replace(" ", "-")
+            .replace("ä", "a")
+            .replace("ö", "o") + ".html";
+
+        if (pageName === "italien-food.html") pageName = "italien-food.html";
+        if (pageName === "swedish-food.html") pageName = "swedish-food.html";
+        if (pageName === "american-food.html") pageName = "american-food.html";
+        if (pageName === "greek-food.html") pageName = "greek-food.html";
+        if (pageName === "persian-food.html") pageName = "persian-food.html";
+        if (pageName === "asian-food.html") pageName = "asian-food.html";
+        if (pageName === "indian-food.html") pageName = "indian-food.html";
+
+        window.location.href = pageName;
+      });
     });
   })
   .catch((error) => {
     console.error("Error loading categories:", error);
   });
- 
+
+window.location.href = category.page; 
