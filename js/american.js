041233046842c433fe12b57657a9bbdf49c121a8
tyraedin <edty24pr@student.ju.se>
2025-04-25 13:13:50 +0200

    Almost done with all the category pages - Added all the pictures to the code using JSON - Made JS, JSON and CSS files for each page and made the structure more complete - Used JSON and JS to load the data in the JSON file in HTML and put the pictures in the website

---
+++ js/american.js
@@ -0,0 +1,24 @@
+fetch("data/american.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const container = document.getElementById("americanFoodContainer");
+
+    data.foods.forEach((food) => {
+      const card = document.createElement("div");
+      card.className = "american-food-card";
+
+      const img = document.createElement("img");
+      img.src = food.image;
+      img.alt = food.name;
+
+      const title = document.createElement("h3");
+      title.textContent = food.name;
+
+      card.appendChild(img);
+      card.appendChild(title);
+      container.appendChild(card);
+    });
+  })
+  .catch((error) => {
+    console.error("Error loading Indian foods:", error);
+  });
