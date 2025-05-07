a4e11913efcf6b35d654cae09a89d90c36f0905c
Arad190783 <noar24xe@student.ju.se>
2025-05-07 17:24:38 +0200

    The search bar is working now and there is just one part which I will work on and that is to jump to the page that is searched in the search bar.

--- js/main.js
+++ js/main.js
@@ -42,4 +42,74 @@ fetch("data/categories.json")
     console.error("Error loading categories:", error);
   });
 
-window.location.href = category.page; 
+/* window.location.href = category.page;
+ */
+let recipes = [
+  "Chicken Tikka Massala",
+  "Korma",
+  "Butter Chicken",
+  "Chana Massala",
+  "Palak Paneer",
+  "Pasta Bolognese",
+  "Pasta Carbonara",
+  "Pasta Pomdoro",
+  "Pepperoni Pizza",
+  "Calzone",
+  "Vesuvio Pizza",
+  "Ribs",
+  "Burger",
+  "Mac & Cheese",
+  "Casserole",
+  "Tacos",
+  "Buffalo Wings",
+  "Chicken Fried Rice",
+  "Chicken Cashew",
+  "Pad Thai",
+  "Nasi Goreng",
+  "Beef & Broccoli",
+  "Spring Rolls",
+  "Meatballs",
+  "Falu Sausage",
+  "Blood Pudding",
+  "Kalops",
+  "Pickled Herring",
+  "Sandwich Cake",
+  "Ghorme Sabzi(stew with rice)",
+  "Gheime Bademjan(stew with rice",
+  "Tahchin Morgh",
+  "Zereshk Polo Ba Morgh",
+  "Fesenjoon",
+  "Lubia Polo",
+];
+
+const resultBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function () {
+  let result = [];
+  let input = inputBox.value;
+  if (input.length) {
+    result = recipes.filter((keyword) => {
+      return keyword.toLowerCase().includes(input.toLowerCase());
+    });
+    console.log(result);
+  }
+  display(result);
+
+  if (!result.length) {
+    resultBox.innerHTML = "";
+  }
+};
+
+function display(result) {
+  const content = result.map((list) => {
+    return "<li onclick = selectInput(this)>" + list + "</li>";
+  });
+
+  resultBox.innerHTML = "<ul>" + content.join("") + "</ul>";
+}
+
+function selectInput(list) {
+  inputBox.value = list.innerHTML;
+  resultBox.innerHTML = "";
+}
